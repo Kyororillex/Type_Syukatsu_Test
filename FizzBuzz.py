301104eada41018bafdc924a0b94299384010b90
Kyohei <gyoroemon0929@gmail.com>
2022-05-12 18:28:41 +0900

    テストできた

---
+++ FizzBuzz.py
@@ -0,0 +1,21 @@
+
+
+for num in range(1, 101):
+    string = ''
+
+    # ここから記述
+    
+    string = num
+
+    if num % 15 ==0:
+        string = "FizzBuzz"
+
+    elif num % 3 == 0:
+        string = "Fizz"
+
+    elif num % 5 == 0:
+        string = "Buzz"
+
+    # ここまで記述
+
+    print(string)
